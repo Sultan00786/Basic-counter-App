c8a823d9a2050df548cdf12d2f65b37df3330f45
Sultan00786 <treeloversultan0987@gmail.com>
2024-01-10 15:42:38 +0530

    compliction of counter app using useState hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,80 @@
+import { useState } from "react";
 import "./App.css";
+import { IoMdAddCircle, IoIosRemoveCircle, IoMdRemoveCircle } from "react-icons/io";
 
 function App() {
-  return (
-    <div>
-      <div>
-        <div>
 
+  const [numValue, setNumValue] = useState(0);
+
+  const increamenValue = () => {
+    if( numValue<100 ){
+      setNumValue(numValue+1);
+    }
+    else{
+      setNumValue(0);
+    }
+  }
+
+  const decreamentValue = () => {
+    if( numValue>-100 ){
+      setNumValue(numValue-1);
+    }
+    else{
+      setNumValue(0);
+    }
+  }
+
+  const resetValue = () => {
+    setNumValue(0)
+  }
+
+
+  return (
+    <div style={{ userSelect: "none" }} className=" flex  justify-center bg-purple-light text-2xl w-[100%] h-[100vh] font-kanit " >
+
+      <div className=" flex flex-col gap-5 items-center " >
+
+        <div className=" bg-mikly-white border-purple-dark border-[3px] px-5 py-3 rounded-md mb-10 mt-44 " >
+          Increment & Decrement
         </div>
-        
+
+        <div className="flex flex-row gap-3 w-full justify-between  " >
+
+            {/* Decrease */}
+            <div 
+              onClick={
+                () => (decreamentValue())
+              }
+              className=" text-4xl bg-purple-dark px-5 py-2 text-mikly-white border-mikly-white border-[3px] rounded-l-full cursor-pointer transition-all duration-200 hover:scale-95 " 
+            >
+              <IoMdRemoveCircle/>
+            </div>
+
+            <div className=" font-lemon text-5xl " >
+              {numValue}
+            </div>
+
+            {/* Increase */}
+            <div
+              onClick={
+                () => (increamenValue())
+              }
+              className=" text-4xl bg-purple-dark px-5 py-2 text-mikly-white border-mikly-white border-[3px] rounded-r-full cursor-pointer transition-all duration-200 hover:scale-95 " 
+            >
+              <IoMdAddCircle />
+            </div>
+        </div>
+
+        <div
+          className=" text-3xl bg-purple-dark px-5 py-2 text-mikly-white mt-5 rounded-lg border-mikly-white border-[3px] cursor-pointer transition-all duration-200 hover:scale-110 "
+          onClick={
+            () => ( resetValue() )
+          }
+        >
+          Reset
+        </div>
+
+
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "purple-light": "#AEAED3",
+        "purple-dark": "#0D1122",
+        "mikly-white": "#E0D8CD",
+      },
+      fontFamily: {
+        "kanit": ['Kanit', 'sans-serif'],
+        "lemon": ['Lemon', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 };
